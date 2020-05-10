c3a72d0872d02065741639ca4b73633f440b1ba4
Diogo <diogogyn100@gmail.com>
2020-05-10 17:17:58 -0300

    first

---
+++ src/main/java/com/example/demo/UdemyApiResTfulSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyApiResTfulSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyApiResTfulSpringApplication.class, args);
+	}
+
+}
